cbe57ecbc674470cb4a4e3cc51f1c682bc155dea
Gi so jeong <sgi716536@gmail.com>
2026-05-23 14:41:44 +0900

    20260523
    20260523

--- 02-javascript-basics/02-dom-events/main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn');
-const msg = document.getElementById('msg');
-let count = 0;
-btn.addEventListener('click', () => {
-  count += 1;
-  msg.textContent = `Clicked ${count} time(s)`;
-});
